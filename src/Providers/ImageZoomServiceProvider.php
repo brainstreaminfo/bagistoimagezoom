3448879ae8a4a14ca87b8155d652f812933e711d
brainstreaminfo <info@brainstream.tech>
2024-08-16 10:03:47 +0530

    changes in service provider file
    changes in service provider file

--- src/Providers/ImageZoomServiceProvider.php
+++ src/Providers/ImageZoomServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Webkul\ImageZoom\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\View;
 
 class ImageZoomServiceProvider extends ServiceProvider
@@ -23,10 +22,6 @@ class ImageZoomServiceProvider extends ServiceProvider
         View::composer('shop::products.view.gallery', function ($view) {
             $view->setPath(base_path('packages/Webkul/ImageZoom/src/Resources/views/products/view/gallery.blade.php'));
         });
-
-        Event::listen('bagisto.admin.layout.head', function($viewRenderEventManager) {
-            $viewRenderEventManager->addTemplate('imagezoom::admin.layouts.style');
-        });
     }
 
     /**
@@ -35,16 +30,6 @@ class ImageZoomServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
-    {
-        $this->registerConfig();
-    }
-
-    /**
-     * Register package config.
-     *
-     * @return void
-     */
-    protected function registerConfig()
     {
         //
     }
